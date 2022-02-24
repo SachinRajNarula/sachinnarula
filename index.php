c5ab2d90f39d37eb7d04b18d88d11da937307e36
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-02-24 12:27:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include "header.php";?>
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
