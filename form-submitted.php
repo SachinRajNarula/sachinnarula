1e459a54b7b1e5b41459b5d47a145a7217f9665c
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-02-23 23:04:28 -0500

    Environment Variable

--- form-submitted.php
+++ form-submitted.php
@@ -1,5 +1,4 @@
 <?php
-// require('dotenv').config();
 
 if (isset($_POST['submit'])){
 
@@ -27,7 +26,7 @@ $mail->Host = "smtp.sendgrid.net";
 $mail->Port = '25';
 $mail->AddAddress($mailid);
 $mail->Username ="apikey";
-$mail->Password ="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";
+$mail->Password ="process.env.API_KEY";
 $mail->SetFrom($email,$fname);
 $mail->AddReplyTo($email,$fname);
 $mail->Subject = $subject;
