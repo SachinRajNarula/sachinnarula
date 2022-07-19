35f36d5d9d29328d9c6d953cd11a49520aea5a9a
Sachin-Raj-Narula <jerryraj40@gmail.com>
2022-07-18 21:51:20 -0400

    PhpMailer stable

--- form-submitted.php
+++ form-submitted.php
@@ -1,48 +1,44 @@
-<?php include "header.php";?>
-Thank you for your submission, <?php echo $_POST["name"]; ?>.
 <?php
-// require('dotenv').config();
-
+require('dotenv').config();
+require "PHPMailer/PHPMailerAutoload.php";
 	$fname = $_POST['name'];
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 
-// require_once 'phpmailer/class.phpmailer.php';
-// /* creates object */
-// $mail = new PHPMailer(true);
-// $mailid = "jerryraj40@gmail.com";
-// $subject = "Form Submitted";
-// $text_message = "Hello";
-// $body = nl2br("Full Name: $fname\nEmail: $email\nSubject: $subject\nMessage: $message");
-
-// try
-// {
-// $mail->IsSMTP();
-// $mail->isHTML(true);
-// $mail->SMTPDebug = 0;
-// $mail->SMTPAuth = true;
-// $mail->SMTPSecure = "tls";
-// $mail->Host = "smtp.sendgrid.net";
-// $mail->Port = '25';
-// $mail->AddAddress($mailid);
-// $mail->Username ="apikey";
-// $mail->Password ="process.API_KEY";
-// $mail->SetFrom($email,$fname);
-// $mail->AddReplyTo($email,$fname);
-// $mail->Subject = $subject;
-// $mail->Body = $body;
-// $mail->AltBody = $body;
-// $mail->Send();
-// if($mail->Send())
-// {
-echo "Thank you for your message. I will respond back ASAP.";
-// }
-// }
-// catch(phpmailerException $ex)
-// {
-// $msg = "
-// ".$ex->errorMessage()."
-// ";
-// }
+function smtpmailer($fname, $email, $subject, $message)
+{
+/* creates object */
+$mail = new PHPMailer();
+$mailid = "jerryraj40@gmail.com";
+$subject = "Form Submitted";
+$text_message = "Hello";
+$body = nl2br("Full Name: $fname\nEmail: $email\nSubject: $subject\nMessage: $message");
+$mail->IsSMTP();
+$mail->isHTML(true);
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "tls";
+$mail->Host = "smtp.sendgrid.net";
+$mail->Port = '25';
+$mail->AddAddress($mailid);
+$mail->Username ="apikey";
+$mail->Password ="process.API_KEY";
+$mail->SetFrom($email,$fname);
+$mail->AddReplyTo($email,$fname);
+$mail->Subject = $subject;
+$mail->Body = $body;
+$mail->AltBody = $body;
+$mail->Send();
+if($mail->Send())
+{
+$error = "Thank you for your message. I will respond back ASAP.";
+ return $error;
+}
+else
+{
+	$error = "No Thank you for your message. I will respond back ASAP.";
+ return $error;
+}
+}
 ?>
